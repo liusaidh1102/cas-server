058762630ffca1770807399b23e0f96678c1af6c
lzw <3182565998@qq.com>
2025-12-08 16:56:01 +0800

    使用默认页面

---
+++ READ_ME_CN.md
@@ -0,0 +1,8 @@
+
+
+# 配置数据库查询密码
+./src/main/resources/application.yml文件中修改数据库配置即可
+# 信息的显示
+若要修改页面上显示的信息，或者是返回的错误信息，请修改./src/main/resources/messages_zh_CN.properties文件中的内容；
+Spring Boot / CAS 的国际化（i18n）机制
+
--- src/main/java/org/apereo/cas/config/CasOverlayOverrideConfiguration.java
+++ src/main/java/org/apereo/cas/config/CasOverlayOverrideConfiguration.java
@@ -1,23 +1,29 @@
 package org.apereo.cas.config;
 
-//import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-//import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @AutoConfiguration
-//@EnableConfigurationProperties(CasConfigurationProperties.class)
 public class CasOverlayOverrideConfiguration {
 
-    /*
-    @Bean
-    public MyCustomBean myCustomBean() {
-        ...
-    }
+    /**
+     * 自定义视图解析器配置
+     * 用于支持标准的CAS登录页面
      */
+    @Bean
+    public WebMvcConfigurer customWebMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                // 使用标准的CAS登录页面而不是自定义页面
+                registry.addViewController("/login").setViewName("casLoginView");
+            }
+        };
+    }
 }
