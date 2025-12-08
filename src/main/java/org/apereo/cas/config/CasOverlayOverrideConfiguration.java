c0c466f97f6738645f0debe7d930d07a46b7b4ff
lzw <3182565998@qq.com>
2025-12-08 16:58:34 +0800

    默认页面

--- src/main/java/org/apereo/cas/config/CasOverlayOverrideConfiguration.java
+++ src/main/java/org/apereo/cas/config/CasOverlayOverrideConfiguration.java
@@ -12,18 +12,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @AutoConfiguration
 public class CasOverlayOverrideConfiguration {
 
-    /**
-     * 自定义视图解析器配置
-     * 用于支持标准的CAS登录页面
-     */
-    @Bean
-    public WebMvcConfigurer customWebMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                // 使用标准的CAS登录页面而不是自定义页面
-                registry.addViewController("/login").setViewName("casLoginView");
-            }
-        };
-    }
 }
